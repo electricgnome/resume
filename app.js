194ab6f4b9e198ecb6246e79d4a4d10cab0d992d
Hector <hector@electricgnome.com>
2018-06-13 14:23:54 -0500

    template

---
+++ app.js
@@ -0,0 +1,101 @@
+// converted to use sequelize instead of pgp
+const express = require("express");
+nunjucks = require("nunjucks");
+body_parser = require("body-parser");
+jsonfile = "/src/file.json";
+const Promise = require("bluebird");
+session = require("express-session");
+redis = require("redis"),
+client = redis.createClient();
+RedisStore = require("connect-redis")(session);
+
+
+
+
+var app = express();
+
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+
+
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+var hour = 3600000;
+app.use(
+  session({
+    store: new RedisStore(),
+    secret: process.env.SECRET_KEY || "dev",
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * hour }
+  })
+);
+const sharedsession = require("express-socket.io-session");
+
+io.use(sharedsession(session({
+  store: new RedisStore(),
+  secret: process.env.SECRET_KEY || "dev",
+  resave: true,
+  saveUninitialized: false,
+  cookie: { maxAge: 24 * hour }
+})));
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  noCache: true
+});
+
+
+
+//Todo APPl
+
+app.get("/", function(request, response) {
+  response.render("about.html");
+});
+
+
+//===========chat app----------------
+
+app.use("/socket-io", express.static("node_modules/socket.io-client/dist"));
+
+var users=[];
+io.on("connection", function(client) {
+  // console.log(client.id + " CONNECTED");
+  // client.emit("message", "Welcome!")
+  // client.users=[];  
+  // io.emit("users", client.users)
+
+  client.on('user', function(user){
+    client.username=user;
+    console.log("user: " + client.username)
+    if (!users.includes(user)){
+      users.push(user)
+    }
+    
+    io.emit("users", users)
+    console.log("list of users: " + users)
+  })
+
+
+  client.on("incoming", function(msg, user) {
+    io.emit("chat-msg", user, msg);
+  });
+
+  
+
+  client.on("disconnect", function(user) {
+    client.emit("message", client.username + " has left the room.")
+    console.log(client.username + " EXITED");
+  });
+});
+//===============-------------------------
+
+
+
+http.listen(8800, function() {
+  console.log("Listening on port 8800");
+});
---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+
+let email_name = $("#user").attr("value");
+var v_user = email_name.split("@")[0];
+if (v_user !== "") {
+  localStorage.setItem("user", v_user);
+}
+user = localStorage.getItem("user");
+var user_li = [];
+// date1.setDate(date1.getDate()+1);
+
+// =====chat app ==========
+var server = io();
+server.on("connect", function(socket) {
+  console.log(user + " connected");
+});
+server.emit("user", user);
+
+server.on("message", function(message) {
+  var chat = document.getElementById("chat-box");
+  chat.insertAdjacentHTML("beforeend", "\n" + message);
+  alert("Server says: " + message);
+});
+
+server.on("users", function(users) {
+  users.forEach(element => {
+    console.log(user_li);
+
+    if (!user_li.includes(element)) {
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(element);
+      node.appendChild(textnode);
+      document.getElementById("user_list").appendChild(node);
+      user_li.push(element);
+    }
+  });
+});
+server.on("chat-msg", function(user, msg) {
+  var chat = document.getElementById("chat-box");
+  chat.insertAdjacentHTML("beforeend", "\n" + user + ": " + msg);
+});
+
+function send_message(event) {
+  var char = event.which || event.keyCode;
+  if (char == "13") {
+    var msg = document.getElementById("message");
+    server.emit("incoming", msg.value, user);
+    msg.value = "";
+  }
+}
+// =====end chat app =========
+
+function materialize() {
+ 
+  $(".tabs").tabs({});
+
+  $(".tooltipped").tooltip();
+  $(".sidenav").sidenav();
+  $(".fixed-action-btn").floatingActionButton({
+    direction: "left",
+    hoverEnabled: false
+  });
+
+  $("input.counter").characterCounter();
+
+  
+}
+
+
+$(window).on("load", function() {
+  materialize();
+  $(".collapsible").collapsible();
+
+ 
+  });
+
