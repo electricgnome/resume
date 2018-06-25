68605e4bb45f7874be81fd449ea8c61dc5948511
Hector <hector@electricgnome.com>
2018-06-25 11:25:41 -0500

    formatting

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ RedisStore = require("connect-redis")(session);
 var app = express();
 
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
-
 
 
 app.use(body_parser.urlencoded({ extended: true }));
@@ -33,15 +31,6 @@ app.use(
     cookie: { maxAge: 24 * hour }
   })
 );
-const sharedsession = require("express-socket.io-session");
-
-io.use(sharedsession(session({
-  store: new RedisStore(),
-  secret: process.env.SECRET_KEY || "dev",
-  resave: true,
-  saveUninitialized: false,
-  cookie: { maxAge: 24 * hour }
-})));
 
 nunjucks.configure("views", {
   autoescape: true,
@@ -59,41 +48,6 @@ app.get("/", function(request, response) {
 
 
 //===========chat app----------------
-
-app.use("/socket-io", express.static("node_modules/socket.io-client/dist"));
-
-var users=[];
-io.on("connection", function(client) {
-  // console.log(client.id + " CONNECTED");
-  // client.emit("message", "Welcome!")
-  // client.users=[];  
-  // io.emit("users", client.users)
-
-  client.on('user', function(user){
-    client.username=user;
-    console.log("user: " + client.username)
-    if (!users.includes(user)){
-      users.push(user)
-    }
-    
-    io.emit("users", users)
-    console.log("list of users: " + users)
-  })
-
-
-  client.on("incoming", function(msg, user) {
-    io.emit("chat-msg", user, msg);
-  });
-
-  
-
-  client.on("disconnect", function(user) {
-    client.emit("message", client.username + " has left the room.")
-    console.log(client.username + " EXITED");
-  });
-});
-//===============-------------------------
-
 
 
 http.listen(8800, function() {
